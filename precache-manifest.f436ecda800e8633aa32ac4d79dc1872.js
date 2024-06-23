b49412f1386388c769f7918cdfdd7be9d6e4a1ec
Maxtanie Petit Dol <108846141+maxtanielab@users.noreply.github.com>
2024-06-23 20:16:37 +0200

    First commit

---
+++ precache-manifest.f436ecda800e8633aa32ac4d79dc1872.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e3b3436e0eb14f0831e44e05fffefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6805328fd3df3a27bd6f",
+    "url": "/static/css/main.3537e348.chunk.css"
+  },
+  {
+    "revision": "0f01a137d6c4b91aa7d1",
+    "url": "/static/js/2.6cc346cf.chunk.js"
+  },
+  {
+    "revision": "b1005de5b6b6ba7fcf6a4d551ecdedc8",
+    "url": "/static/js/2.6cc346cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6805328fd3df3a27bd6f",
+    "url": "/static/js/main.7e834d89.chunk.js"
+  },
+  {
+    "revision": "2ec43b5d52842e163b55",
+    "url": "/static/js/runtime-main.80c95444.js"
+  },
+  {
+    "revision": "306bab4253d1ff0e785f78771913b6fa",
+    "url": "/static/media/750x578.306bab42.png"
+  },
+  {
+    "revision": "b12d7dca411657fd1645b9db83e4573d",
+    "url": "/static/media/Lazer84.b12d7dca.ttf"
+  },
+  {
+    "revision": "2b4777ccae990731c94ea834df3f503b",
+    "url": "/static/media/NotoSansJP-Black.2b4777cc.otf"
+  },
+  {
+    "revision": "3db31919b255ed7261e7329f0f8303a4",
+    "url": "/static/media/NotoSansJP-Bold.3db31919.otf"
+  },
+  {
+    "revision": "5da08a6f2f4e08e906fe93ba44232278",
+    "url": "/static/media/NotoSansJP-Light.5da08a6f.otf"
+  },
+  {
+    "revision": "366120c9b4029066302ca6d75afb6f8b",
+    "url": "/static/media/NotoSansJP-Medium.366120c9.otf"
+  },
+  {
+    "revision": "b5f8ef0cb10cf4fa6dfc584d29908421",
+    "url": "/static/media/NotoSansJP-Regular.b5f8ef0c.otf"
+  },
+  {
+    "revision": "b2aae66e06d9d23604f17240ae03d07a",
+    "url": "/static/media/NotoSansJP-Thin.b2aae66e.otf"
+  },
+  {
+    "revision": "05d62afd6aa526a20a5d66c18dccae2f",
+    "url": "/static/media/appli-mob-titi-burger.05d62afd.jpg"
+  },
+  {
+    "revision": "788f54f81c35a8834d8ebad8ae12b884",
+    "url": "/static/media/behance-icon.788f54f8.svg"
+  },
+  {
+    "revision": "b40fe3d6601685c5851815acd71b1699",
+    "url": "/static/media/behance-white.b40fe3d6.svg"
+  },
+  {
+    "revision": "fdd007d7d66d05e1d36f4e768ec0d859",
+    "url": "/static/media/behance-yellow.fdd007d7.svg"
+  },
+  {
+    "revision": "d661ed14a12438f70af327a75276ee60",
+    "url": "/static/media/behance.d661ed14.svg"
+  },
+  {
+    "revision": "496ceea518a1018daa414a41c31eaf04",
+    "url": "/static/media/carte-collect.496ceea5.jpg"
+  },
+  {
+    "revision": "ba6c1b03734f42d787f46e0b15a5faa8",
+    "url": "/static/media/carte-visite-emilie.ba6c1b03.jpg"
+  },
+  {
+    "revision": "8b8994006e8f53502301cc2141e3ed26",
+    "url": "/static/media/chabadou.8b899400.png"
+  },
+  {
+    "revision": "ccb231a6769f09de51f4aff676103749",
+    "url": "/static/media/emilie-1224.ccb231a6.png"
+  },
+  {
+    "revision": "bc15d078038e0875eb36cadc78d6d851",
+    "url": "/static/media/emilie-500.bc15d078.png"
+  },
+  {
+    "revision": "86a9c51384f01899e4dd4c2893826f32",
+    "url": "/static/media/emilie-768.86a9c513.png"
+  },
+  {
+    "revision": "d94a2ba65c611599a4f469c963cb623b",
+    "url": "/static/media/emilie.d94a2ba6.png"
+  },
+  {
+    "revision": "53a10b4895afa1e6c64a543344719ffe",
+    "url": "/static/media/icone-victoir.53a10b48.jpg"
+  },
+  {
+    "revision": "dcf7fb5ec0c219e3bead1999f1254364",
+    "url": "/static/media/illustration.dcf7fb5e.jpg"
+  },
+  {
+    "revision": "b4dc3eb2cdd1a326ab998cbeedc22152",
+    "url": "/static/media/indentite.b4dc3eb2.jpg"
+  },
+  {
+    "revision": "edacb7b7283f39a614611a5657f2ebd3",
+    "url": "/static/media/instagram-icon.edacb7b7.svg"
+  },
+  {
+    "revision": "43ee1b951ca0b97f9e45e18b18f210ef",
+    "url": "/static/media/instagram-white.43ee1b95.svg"
+  },
+  {
+    "revision": "7615cf2bd83c27a535da2b21824b325a",
+    "url": "/static/media/instagram-yellow.7615cf2b.svg"
+  },
+  {
+    "revision": "9a873995234a1d69237fb223217e7339",
+    "url": "/static/media/instagram.9a873995.svg"
+  },
+  {
+    "revision": "49d350fa70b39d87e7186bbce88343ea",
+    "url": "/static/media/karina-colors.49d350fa.jpg"
+  },
+  {
+    "revision": "d6e74702ecbdd401b5c42c656f9e6921",
+    "url": "/static/media/karina-photo.d6e74702.jpg"
+  },
+  {
+    "revision": "bf04bed8bf411d76a56ff08641cbd85c",
+    "url": "/static/media/linkdin.bf04bed8.svg"
+  },
+  {
+    "revision": "5562d8a9ee1c1c49f2d432a8530168e3",
+    "url": "/static/media/linkedin-icon.5562d8a9.svg"
+  },
+  {
+    "revision": "007808eba81179ddd4a992ec1caf3e68",
+    "url": "/static/media/linkedin-white.007808eb.svg"
+  },
+  {
+    "revision": "99741db47d68979bd4b9ab4896ac610f",
+    "url": "/static/media/linkedin-yellow.99741db4.svg"
+  },
+  {
+    "revision": "a040518a692a2112a8d03557bb020122",
+    "url": "/static/media/logo-blanc-2.a040518a.png"
+  },
+  {
+    "revision": "ddb5691b71fb28c0afc06af9b6951682",
+    "url": "/static/media/logo-blanc.ddb5691b.png"
+  },
+  {
+    "revision": "e9ea166b432aaeb2d7361d4ed9d00aa8",
+    "url": "/static/media/logo.e9ea166b.png"
+  },
+  {
+    "revision": "91eb3a2f33c641290efc9dd1bed8d77e",
+    "url": "/static/media/maquette-paris.91eb3a2f.jpg"
+  },
+  {
+    "revision": "9366bebcfc1c61f837ab41a9f45a9f1a",
+    "url": "/static/media/mode-1.9366bebc.png"
+  },
+  {
+    "revision": "fbb957c7bb975047d5454ff1d6c203a5",
+    "url": "/static/media/oldeez.fbb957c7.png"
+  },
+  {
+    "revision": "adf4dea99d4af24261073e25d05a5e5e",
+    "url": "/static/media/outil-cintic.adf4dea9.jpg"
+  },
+  {
+    "revision": "9fa79134aab07084f427aa87f361f174",
+    "url": "/static/media/personnage-poon.9fa79134.jpg"
+  },
+  {
+    "revision": "a341cd807521b96d216c742caaaec56d",
+    "url": "/static/media/pexels-moose-photos-1036641.a341cd80.jpg"
+  },
+  {
+    "revision": "b1264c0a78acc518c8d8194865ee371e",
+    "url": "/static/media/poon.b1264c0a.png"
+  },
+  {
+    "revision": "5056319a51177d5fc7640bceef071714",
+    "url": "/static/media/presentation-titi-burger-2020-maquette.5056319a.jpg"
+  },
+  {
+    "revision": "df6365338a27dd231e7b03a62755dfb3",
+    "url": "/static/media/print.df636533.jpg"
+  },
+  {
+    "revision": "e686195242e74e973a73385f7c2e59c2",
+    "url": "/static/media/web.e6861952.jpg"
+  },
+  {
+    "revision": "3e65ecf847c1e4ec3888343ada133375",
+    "url": "/static/media/youtube-icon.3e65ecf8.svg"
+  },
+  {
+    "revision": "3c7910053636c05258d39a6c2c4c09a0",
+    "url": "/static/media/youtube.3c791005.svg"
+  }
+]);
